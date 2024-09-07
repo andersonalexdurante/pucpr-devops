e8d1ec1d7e322dec1429cf18c81cfcfd46ce156c
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2024-09-07 12:01:07 -0300

    Adiciona Swagger no projeto

--- src/main/java/com/pucpr/devops/resources/DevOpsResource.java
+++ src/main/java/com/pucpr/devops/resources/DevOpsResource.java
@@ -1,5 +1,6 @@
 package com.pucpr.devops.resources;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DevOpsResource {
     @GetMapping("v1")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Primeiro Resource")
     public String primeiroResource() {
         return "Aplicação PUCPR DevOps";
     }
